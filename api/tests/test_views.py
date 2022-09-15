f7d2e0c0e04a17d7c70504142191c3dfe6a43ebb
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-09-15 03:15:14 +0300

    ensure that get_city_temp view returns a response

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,4 +1,18 @@
-from django.test import TestCase, Client
+from urllib import response
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# initialize the APIClient app
-client = Client()
+
+class WeatherAPITestCase(APITestCase):
+    """
+    Test module for Weather API 
+    """
+    def get_city_temp(self):
+        """
+        Ensure we can get an API response
+        """
+        url = reverse('get_city_temp')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
