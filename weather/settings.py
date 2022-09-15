2c6f0f3e0429786ebfd9bab38728d95a7e3daef7
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-09-15 03:43:26 +0300

    api key env variable

--- weather/settings.py
+++ weather/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# access env variables
+env = environ.Env()
+environ.Env.read_env()
+
+WEATHER_API_KEY = env('WEATHER_API_KEY')
 
 
 # Quick-start development settings - unsuitable for production
