daa0640b6aeba481024cb3c6e4462af013b84346
Munyoki Kilyungi <me@bonfacemunyoki.com>
2022-09-24 17:48:29 +0300

    Rewrite how endpoint for getting temperatures
    * api/tests/test_weather.py (WeatherAPITestCase): Remove tests that
    rely on a network connection.
    * api/views.py (get_city_temp): Refactor.

--- api/tests/test_weather.py
+++ api/tests/test_weather.py
@@ -1,48 +1,19 @@
-from urllib import response
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from rest_framework import status
 
 
 class WeatherAPITestCase(APITestCase):
     """
-    Test module for Weather API 
+    Test module for Weather API
     """
-
-    def get_city_temp(self):
-        """
-        Ensure we can get an API response
-        """
-        url = reverse('get_city_temp', kwargs={
-                      'city': 'London'})
-        response = self.client.get(url, {'days': 10})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_invalid_city(self):
-        """
-        When a non existent city is queried.
-        """
-        url = reverse('get_city_temp', kwargs={
-                      'city': 'Lo'})
-
-        response = self.client.get(url, {'days': 10})
-        result = response.json()
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(result['error']['code'], 1006)
-        self.assertEqual(result['error']['message'],
-                         "No matching location found.")
-
     def test_invalid_period(self):
         """
         When invalid period is provided
         """
-        url = reverse('get_city_temp', kwargs={
-                      'city': 'London'})
+        url = reverse("get_city_temp", kwargs={"city": "London"})
 
-        response = self.client.get(url, {'days': 'ten'})
+        response = self.client.get(url, {"days": "ten"})
         result = response.json()
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(result['message'], "Value provided is not an integer")
-
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["message"], "Value provided is not an integer")
--- api/views.py
+++ api/views.py
@@ -1,51 +1,29 @@
 import requests
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from django.conf import settings
 
-from api.utils import WEATHER_API_BASE_URL, get_average, get_data, get_median
-
-# Create your views here.
+from api.utils import WeatherData
 
 
 @api_view()
 def get_city_temp(request, city):
+    if not str(request.GET.get("days")).isnumeric():
+        return Response({"message": "Value provided is not an integer"}, 400)
 
-    # get api key
-    api_key = settings.WEATHER_API_KEY
-
-    # get days
-    days = request.GET.get('days')
-
-    # check if input value is valid
-    if str(days).isnumeric():
-        # structure url
-        url = WEATHER_API_BASE_URL + "/forecast.json?key="+api_key + \
-            "&q=" + city + "&days="+days+"&aqi=no&alerts=no"
-
-        try:
-            response = requests.get(url)
-            # if response was successful
-            result = response.json()
-        except requests.exceptions.Timeout as e:
-            print("Timeout error:", e)
-        except requests.HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')  
-        except Exception as err:
-            print(f'Other error occurred: {err}')  
-
-        if response.status_code == 200:
-            data = get_data(result)
-            return Response({
-                "maximum": max(data),
-                "minimum": min(data),
-                "average": get_average(data),
-                "median": get_median(data),
-            })
-        else:
-            return Response(result)
-
+    response = requests.get(
+        f"{settings.WEATHER_API_BASE_URL}/"
+        f"forecast.json?key={settings.WEATHER_API_KEY}"
+        f"&q={city}&days{request.GET.get('days')}&aqi=no&alerts=no"
+    )
+    try:
+        response.raise_for_status
+    except requests.exceptions.HTTPError:
+        # Contains the appropriate error message
+        return Response(
+            {"error": {"code": 1006, "message": "No matching location found"}},
+            400,
+        )
     else:
-        return Response({"message": "Value provided is not an integer"})
+        return Response(WeatherData(response.json()).data, 200)
